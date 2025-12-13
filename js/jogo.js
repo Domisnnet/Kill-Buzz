7bf99058edd704defc0153294942d67c5a920a7e
Domisnnet <dominique.goncalves61@gmail.com>
2025-12-13 13:22:13 -0300

    alterado

--- js/jogo.js
+++ js/jogo.js
@@ -1,12 +1,10 @@
-
 let altura = 0
 let largura = 0
 let vidas = 1
 let tempo = 15
-
 let criaMosquitoTempo = 1500
-
 let nivel = window.location.search
+
 nivel = nivel.replace('?', '')
 
 if(nivel === 'normal') {
